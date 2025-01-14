64eb2a759e282422f8df252263da86b72cf1f851
Mert Degirmenci <81925072+m-deg@users.noreply.github.com>
2025-01-14 08:36:22 -0800

    Update install_worker.sh

---
+++ install_worker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter GIT access token >" accesstoken
+sudo apt update
+sudo apt install git python3 pip -y
+sudo pip3 install termcolor chardet pytz --break-system-packages
+sudo mkdir -p /datadrive/code
+sudo chmod 777 /datadrive/code
+cd /datadrive/code
+git clone https://m-deg:$accesstoken@github.com/m-deg/projects.git .
+python3 /datadrive/code/common/python/main_program_module.py --install worker all
