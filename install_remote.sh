b5c212fe7ae0f7c7df252435c0174c9e523fb26b
Mert Degirmenci <81925072+m-deg@users.noreply.github.com>
2025-01-16 14:43:52 -0800

    Update install_remote.sh

--- install_remote.sh
+++ install_remote.sh
@@ -2,8 +2,8 @@
 
 read -p "Enter GIT access token >" accesstoken
 sudo apt update
-sudo apt install git python3 pip -y
-sudo pip3 install termcolor chardet pytz --break-system-packages
+sudo apt install git python3 curl pip -y
+sudo pip3 install termcolor chardet pytz filelock --break-system-packages
 cd /datadrive/code
 sudo git pull
 git config --global --add safe.directory /datadrive/code
