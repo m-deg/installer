59de70721547532f58c950471e12d849eef51ad3
root <root@mdegirmenci-primary>
2025-05-16 06:58:11 -0700

    checkin

--- install_local.sh
+++ install_local.sh
@@ -8,12 +8,13 @@
 
 read -p "Enter GIT username >" username
 read -p "Enter GIT access token >" accesstoken
+read -p "Enter your GIT fork repo name >" reponame
 sudo apt update
 sudo apt install git python3 curl pip -y
 sudo pip3 install termcolor chardet pytz filelock --break-system-packages
 sudo mkdir -p /datadrive/code
 sudo chmod 777 /datadrive/code
 cd /datadrive/code
-git clone https://$username:$accesstoken@github.com/degorg/main.git .
+git clone https://$username:$accesstoken@github.com/$username/$reponame.git .
 python3 /datadrive/code/common/python/main_program_module.py --install local all
 
